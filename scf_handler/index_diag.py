e575fd752d7a0fe1c490d98b453f5b40aca8774a
bamazhaobei <bamazhaobei@github.com>
2026-06-08 14:30:13 +0800

    fix: HTML通过JS base64解码渲染

--- scf_handler/index_diag.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import json
-
-def main_handler(event, context):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json; charset=utf-8"},
-        "body": json.dumps({"ok": True, "method": event.get("httpMethod", "N/A"), "keys": sorted(event.keys())}, ensure_ascii=False)
-    }
